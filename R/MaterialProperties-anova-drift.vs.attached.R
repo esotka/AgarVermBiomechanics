dc7b31a81665982e8a4456c07beb4efc80a44091
esotka <eriksotka@gmail.com>
2021-06-18 12:45:53 -0400

    A commit from my local computer

--- R/MaterialProperties-anova-drift.vs.attached.R
+++ R/MaterialProperties-anova-drift.vs.attached.R
@@ -247,10 +247,15 @@ dev.off()
 
 ### breakage
 tmp <- melt(br[,c("site","Continent","natnon","attach3","peak_force")])
+tmp$site_attach <- paste(tmp$site,tmp$attach3)
 tmp2 <- cast(tmp,site+Continent+natnon+attach3~variable,mean,na.rm=T)
-f1 <- ggplot(tmp2,aes(y=peak_force,x=Continent,fill=attach3)) +
+tmp2$sd <- cast(tmp,site+Continent+attach3~variable,sd,na.rm=T)$peak_force
+tmp2$n <- table(tmp$site_attach)
+tmp2$se <- (tmp2$sd)/sqrt(tmp2$n)
+
+f1 <- ggplot(tmp2,aes(y=peak_force,x=Continent,fill=attach3,ymax=peak_force+se,ymin=peak_force-se)) +
   geom_boxplot() +
-  geom_point(pch = 21, position = position_jitterdodge(jitter.width=0.1)) + 
+  geom_pointrange(pch = 21, cex=0.3, position = position_jitterdodge(jitter.width=0.5)) + 
   scale_fill_manual(values=c("white","grey")) +
   ylab("peak force") + xlab("") +
   guides(fill=FALSE) +
@@ -262,10 +267,15 @@ f1 <- ggplot(tmp2,aes(y=peak_force,x=Continent,fill=attach3)) +
 
 ### slope
 tmp <- melt(br[,c("site","Continent","natnon","attach3","slope_Mpa")])
+tmp$site_attach <- paste(tmp$site,tmp$attach3)
 tmp2 <- cast(tmp,site+Continent+natnon+attach3~variable,mean,na.rm=T)
-f2 <- ggplot(tmp2,aes(y=slope_Mpa,x=Continent,fill=attach3)) +
+tmp2$sd <- cast(tmp,site+Continent+attach3~variable,sd,na.rm=T)$slope_Mpa
+tmp2$n <- table(tmp$site_attach)
+tmp2$se <- (tmp2$sd)/sqrt(tmp2$n)
+
+f2 <- ggplot(tmp2,aes(y=slope_Mpa,x=Continent,fill=attach3,ymax=slope_Mpa+se,ymin=slope_Mpa-se)) +
   geom_boxplot() +
-  geom_point(pch = 21, position = position_jitterdodge(jitter.width=0.1)) + 
+  geom_pointrange(pch = 21, cex=0.3, position = position_jitterdodge(jitter.width=0.5)) + 
   scale_fill_manual(values=c("white","grey")) +
   ylab("Slope") + xlab("") +
   guides(fill=FALSE) +
@@ -277,10 +287,14 @@ f2 <- ggplot(tmp2,aes(y=slope_Mpa,x=Continent,fill=attach3)) +
 
 ### maxstress
 tmp <- melt(br[,c("site","Continent","natnon","attach3","maxstress")])
+tmp$site_attach <- paste(tmp$site,tmp$attach3)
 tmp2 <- cast(tmp,site+Continent+natnon+attach3~variable,mean,na.rm=T)
-f3 <- ggplot(tmp2,aes(y=maxstress,x=Continent,fill=attach3)) +
+tmp2$sd <- cast(tmp,site+Continent+attach3~variable,sd,na.rm=T)$maxstress
+tmp2$n <- table(tmp$site_attach)
+tmp2$se <- (tmp2$sd)/sqrt(tmp2$n)
+f3 <- ggplot(tmp2,aes(y=maxstress,x=Continent,fill=attach3,ymax=maxstress+se,ymin=maxstress-se)) +
   geom_boxplot() +
-  geom_point(pch = 21, position = position_jitterdodge(jitter.width=0.1)) + 
+  geom_pointrange(pch = 21, cex=0.3, position = position_jitterdodge(jitter.width=0.5)) + 
   scale_fill_manual(values=c("white","grey")) +
   ylab("Max Stress") + xlab("") +
   guides(fill=FALSE) +
@@ -292,10 +306,15 @@ f3 <- ggplot(tmp2,aes(y=maxstress,x=Continent,fill=attach3)) +
 
 ### maxstrain
 tmp <- melt(br[,c("site","Continent","natnon","attach3","maxstrain")])
+tmp$site_attach <- paste(tmp$site,tmp$attach3)
 tmp2 <- cast(tmp,site+Continent+natnon+attach3~variable,mean,na.rm=T)
-f4 <- ggplot(tmp2,aes(y=maxstrain,x=Continent,fill=attach3)) +
+tmp2$sd <- cast(tmp,site+Continent+attach3~variable,sd,na.rm=T)$maxstrain
+tmp2$n <- table(tmp$site_attach)
+tmp2$se <- (tmp2$sd)/sqrt(tmp2$n)
+
+f4 <- ggplot(tmp2,aes(y=maxstrain,x=Continent,fill=attach3,ymax=maxstrain+se,ymin=maxstrain-se)) +
   geom_boxplot() +
-  geom_point(pch = 21, position = position_jitterdodge(jitter.width=0.1)) + 
+  geom_pointrange(pch = 21, cex=0.3, position = position_jitterdodge(jitter.width=0.5)) + 
   scale_fill_manual(values=c("white","grey")) +
   ylab("Max Strain") + xlab("") +
   guides(fill=FALSE) +
@@ -307,8 +326,12 @@ f4 <- ggplot(tmp2,aes(y=maxstrain,x=Continent,fill=attach3)) +
 
 ### auc_modulus
 tmp <- melt(br[,c("site","Continent","natnon","attach3","auc_modulus")])
+tmp$site_attach <- paste(tmp$site,tmp$attach3)
 tmp2 <- cast(tmp,site+Continent+natnon+attach3~variable,mean,na.rm=T)
-f5 <- ggplot(tmp2,aes(y=auc_modulus,x=Continent,fill=attach3)) +
+tmp2$sd <- cast(tmp,site+Continent+attach3~variable,sd,na.rm=T)$auc_modulus
+tmp2$n <- table(tmp$site_attach)
+tmp2$se <- (tmp2$sd)/sqrt(tmp2$n)
+f5 <- ggplot(tmp2,aes(y=auc_modulus,x=Continent,fill=attach3,ymax=auc_modulus+se,ymin=auc_modulus-se)) +
   geom_boxplot() +
   geom_point(pch = 21, position = position_jitterdodge(jitter.width=0.1)) + 
   scale_fill_manual(values=c("white","grey")) +
